7bbd18d2e7c4d47e795df7bd3bb068ac64baa314
Sylitas <johnluy1999@gmail.com>
2023-05-26 20:28:42 +0700

    Adding frontend

--- index.js
+++ index.js
@@ -11,22 +11,27 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  // TODO: Send homepage here
-  res.send('hello world')
-})
+const domain = process.env.DOMAIN || `${process.env.HOST}:${process.env.PORT}`
+
+app.get('/', (req, res) => { res.sendFile('./public/index.html') })
 
 app.get('/:id', async (req, res) => {
   console.log('😎 Sylitas | Get a old url by using shortener URL ');
   const { id: urlId } = req.params;
-  const data = await mongoDBClient('findOne', { id: urlId });
-  res.redirect(data.userURL);
+  console.log('😎 Sylitas | urlId : ', urlId);
+  if (!urlId || urlId === '') {
+    res.redirect('/')
+  } else {
+    const { userURL } = await mongoDBClient('findOne', { id: urlId });
+    res.redirect(userURL);
+  }
 })
 
 app.post('/', async (req, res) => {
   console.log('😎 Sylitas | Create a shortener URL ');
-  const { url, expiredAfter = 30 } = req.body;
+  const { url, expiredAfter = parseInt(process.env.AFTER_DAYS, 10) } = req.body;
 
   const urlId = Shortid.generate();
 
@@ -35,8 +40,6 @@ app.post('/', async (req, res) => {
     userURL: url,
     expired: generateExpiredDate(expiredAfter)
   });
-
-  const domain = process.env.DOMAIN || `${process.env.HOST}:${process.env.PORT}`
 
   res.status(200).json({ url: `http://${domain}/${urlId}` })
 })
